b4f4773fcae0406a7815186706a26b777cd97ca0
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-12-27 01:16:00 +0000

    new jar and test files

--- src/main/java/br/com/rhribeiro25/Main.java
+++ src/main/java/br/com/rhribeiro25/Main.java
@@ -1,25 +1,25 @@
 package br.com.rhribeiro25;
 
 import br.com.rhribeiro25.presentation.controllers.GameController;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    private static final Random RANDOM = new Random();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
     public static void main(String[] args) throws IOException {
 
-        if (args.length < 2) {
+        if (args.length < 4) {
             System.out.println("To use this application, you need to pass two arguments: " +
-                    "\nUsage: java -jar <your-jar-file> --config <config_matrix[5][5].json> --betting-amount <amount>");
+                    "\nUsage: java -jar <your-jar-file> --config <config_matrix.json> --betting-amount <amount>");
             return;
         }
+        List<String> argsList = Arrays.asList(args);
+        int indexConfig = argsList.indexOf("--config") + 1;
+        int indexBettingAmount = argsList.indexOf("--betting-amount") + 1;
 
-        String configPath = args[0];
-        int bettingAmount = Integer.parseInt(args[1]);
+        String configPath = args[indexConfig];
+        int bettingAmount = Integer.parseInt(args[indexBettingAmount]);
 
 
         if (configPath.isBlank() || bettingAmount <= 0) {
